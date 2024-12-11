414003cb089937b5b1c07a8b9bcfc3776f5b7722
Joshua Conklin <joshuaconk14>
2024-12-10 18:05:05 -0800

    Deploy

--- src/App.js
+++ src/App.js
@@ -37,6 +37,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
+import SpotAuth from "./pages/spotauth";
 
 // , {useState, useEffect}
 
@@ -45,8 +49,12 @@ function App() {
   return (
     <div className = "App">
       <Router>
-        <Routes>
+      <Routes>
           <Route path= "/" element={<Home />} />
+          <Route path= "/login" element={<Login />} />
+          <Route path= "/register" element={<Register />} />
+          <Route path= "/dashboard" element={<Dashboard />} />
+          <Route path= "/spotauth" element={<SpotAuth />} />
         </Routes>
       </Router>
     </div>
